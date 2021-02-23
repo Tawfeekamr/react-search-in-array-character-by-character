82b56aab654dde8ca84611c798f7c449cb0a6a0a
Tawfeeq Amro <tawfeekamr@Tawfeeqs-MacBook-Pro.local>
2021-02-23 20:33:43 +0200

    React search by character

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+    const names = ["John","Steve","Alen","Stephen","Smith","Alex","Jack","Andy","Jacky"];
+
+    const [term, setTerm] = useState('');
+  const [filltredData, setFillterd] = useState(names);
+
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    search(term);
+  };
+
+  const search =  (value) => {
+    if (value) {
+      // console.log(value);
+      let searchResults = [...names].filter(name => {
+            return name.toLowerCase().includes(term.toLowerCase());
+          })
+      setFillterd(searchResults);
+    }
+    if (value === ''){
+        setFillterd(names);
+    }
+  }
+
+  useEffect(() => {
+    search();
+  }, [term]);
+
+    const rerenderData = () => {
+        return  filltredData.map(item => <div key={names.indexOf(item)}>{item}</div>);
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit} className="search-form">
+        <div className="search-field">
+          <input
+              className="search-input"
+              type="text"
+              value={term}
+              onChange={(event) => {
+                setTerm(event.target.value);
+                search(term);
+              }}
+              placeholder="Search here"
+          />
+            {rerenderData()}
+        </div>
+      </form>
     </div>
   );
 }
